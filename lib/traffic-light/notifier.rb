0c1a4f20545635203c1512ac31e2e9bc9a04eb86
Aelphy <miha-usv@yandex.ru>
2014-06-20 19:03:41 +0600

    Added twitter

--- lib/traffic-light/notifier.rb
+++ lib/traffic-light/notifier.rb
@@ -1,6 +1,15 @@
+require 'twitter'
+
 module TrafficLight
   class Notifier
-    URL = 'http://ci.dev.apress.ru/XmlStatusReport.aspx'
+    URL = 'http://ci.railsc.ru/XmlStatusReport.aspx'
+
+    @@client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = 'IRJU5nDfDUOnHfw38wD7VShd2'
+      config.consumer_secret     = 'Y3rDQS0jVdg0gqBOnxYpxPIqAouQopvDswjiV55ahAZISZF80g'
+      config.access_token        = '2433155473-jLYPVqkY7x2crvyeDyhLjzxXFopdS8J0xNpoH9T'
+      config.access_token_secret = 'sdDu9CS829FtXF9rRtqTMcNGlf3e00Zy3FEL6aMgwjgsI'
+    end
 
     # Find build for project
     #
@@ -17,6 +26,18 @@ module TrafficLight
     #
     # Returns Integer
     def notify
+      @previous_activity ||= build.activity
+
+      if @previous_activity != 'Building' && build.activity == 'Building'
+        @@client.update("Запущен билд ветки #{project_name}")
+      elsif @previous_activity == 'Building' && build.activity != 'Building'
+        if build.lastBuildStatus == 'Failure'
+          @@client.update("Билд ветки #{project_name} не прошел")
+        elsif build.lastBuildStatus == 'Success'
+          @@client.update("Билд ветки #{project_name} прошел успешно")
+        end
+      end
+
       if build.activity == 'Building'
         lights.extinguish(lights.yellow)
         sleep 0.25
